a7dbef95f3a54463a749f2858360435d0e33df32
fmowl10 <jinseok1001@hotmail.com>
2016-08-31 17:41:41 +0900

    add something

--- fmowl/Chap 3/Source/systemMonitor/systemMoitor/Form1.cs
+++ fmowl/Chap 3/Source/systemMonitor/systemMoitor/Form1.cs
@@ -8,7 +8,6 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Diagnostics;
-using System.Threading;
 
 namespace systemMoitor
 {
@@ -24,27 +23,26 @@ namespace systemMoitor
             cpu = new PerformanceCounter("Processor", "% Processor Time", "_Total");
             ram = new PerformanceCounter("Process", "Working Set", "_Total");
             i = 0;
-            Thread.Sleep(100);
             timer1.Interval = 500;
             timer1.Start();
         }
         private void Form1_Load(object sender, EventArgs e)
         {
             usageChart.Series.Add("CPU");
-            usageChart.Series.Add("Ram");
+            //usageChart.Series.Add("Ram");
             usageChart.Series[0].IsVisibleInLegend = false;
-            usageChart.Series[1].IsVisibleInLegend = false;
+            //usageChart.Series[1].IsVisibleInLegend = false;
             usageChart.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Area;
-            usageChart.Series[1].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Area;
+            //usageChart.Series[1].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Area;
             usageChart.ChartAreas[0].AxisY.Maximum = 100;
-            usageChart.Series[0].Color = Color.FromArgb(0x0f,0xff,0x00,0x00);
-            usageChart.Series[1].Color = Color.FromArgb(0x0f, 0x00, 0x00, 0xff);
+            usageChart.Series[0].Color = Color.FromArgb(0x40,0xff,0x00,0x00);
+            //usageChart.Series[1].Color = Color.FromArgb(0x0f, 0x00, 0x00, 0xff);
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
             usageChart.Series[0].Points.AddXY(i,(int) cpu.NextValue());
-            usageChart.Series[1].Points.AddXY(i, (ram.NextSample());
+            //usageChart.Series[1].Points.AddXY(i, ram.NextSample());
             i++;
         }
     }
